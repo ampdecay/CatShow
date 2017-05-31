cbeabeedcc76ce3e0f08c26e0519798ce34aca30
Julien Daigle <julzmeister@gmail.com>
2017-05-31 13:49:42 -0400

    Changed Backgroud Color

--- CatShow/CatShow/CatShowMain.Designer.vb
+++ CatShow/CatShow/CatShowMain.Designer.vb
@@ -71,6 +71,7 @@ Partial Class CatShowMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Thistle
         Me.ClientSize = New System.Drawing.Size(670, 469)
         Me.Controls.Add(Me.viewEvents_Button)
         Me.Controls.Add(Me.addEvent_Button)
